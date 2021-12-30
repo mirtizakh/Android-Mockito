9cbc55a6e95b10ace3eabb09c51ae7593e21fefa
M.Irtiza Khursheed <irtizakh@gmail.com>
2021-12-30 07:02:52 +0500

    Addition - Tet cases are added with Mockito

--- AndroidMockito/app/src/test/java/com/mockito/android/test_doubles/Spy/PaymentServiceTest.kt
+++ AndroidMockito/app/src/test/java/com/mockito/android/test_doubles/Spy/PaymentServiceTest.kt
@@ -1,8 +1,12 @@
 package com.mockito.android.test_doubles.Spy
 
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.*
+import org.mockito.kotlin.verify
 
 class PaymentServiceTest {
     private lateinit var operatorRate: IOperatorRate
@@ -21,9 +25,22 @@ class PaymentServiceTest {
 
 
     @Test
-    fun notSendEmailForSalesUnder1000() {
+    fun test_notSendEmailForSalesUnder1000() {
         val spiedPaymentService = PaymentService(loggerDummy, operatorRate, emailSender)
         spiedPaymentService.createPaymentRequest(500, customerName)
         assertEquals(0, emailSender.timesCalled())
+    }
+
+    @Test
+    fun test_notSendEmailForSalesUnder1000_withMockito() {
+       // val mockEmailSender = mock(IPaymentEmailSender::class.java)
+        val mockEmailSender = mockk<IPaymentEmailSender>()
+        val mockPaymentRequest = mockk<PaymentRequest>()
+        val spiedPaymentService = PaymentService(loggerDummy, operatorRate, mockEmailSender)
+        val paymentRequest = spiedPaymentService.createPaymentRequest(1500, customerName)
+        paymentRequest?.let { request->
+           // verify(mockEmailSender, times(1)).send(mockPaymentRequest)
+            verify(exactly = 0) { mockEmailSender.send(mockPaymentRequest) }
+        }
     }
 }
--- AndroidMockito/app/src/test/java/com/mockito/android/test_doubles/fake_test_doubles/FakeBookServiceTest.kt
+++ AndroidMockito/app/src/test/java/com/mockito/android/test_doubles/fake_test_doubles/FakeBookServiceTest.kt
@@ -4,6 +4,9 @@ package com.mockito.android.test_doubles.fake_test_doubles
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.time.LocalDate
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
 
 class FakeBookServiceTest {
 
@@ -16,6 +19,19 @@ class FakeBookServiceTest {
         bookService.addBook(Book("1234", "Mockito In Action", 250, LocalDate.now()))
         bookService.addBook(Book("1235", "JUnit 5 In Action", 200, LocalDate.now()))
 
+        assertEquals(2, bookService.findNumberOfBooks())
+    }
+
+    @Test
+    fun test_FakeWithMockito() {
+        val bookRepository: IBookRepository = mock(IBookRepository::class.java)
+        val bookService = BookService(bookRepository)
+        val book1 = Book("1234", "Mockito In Action", 250, LocalDate.now())
+        val book2 = Book("1235", "JUnit 5 In Action", 200, LocalDate.now())
+        val books: MutableCollection<Book> = ArrayList()
+        books.add(book1)
+        books.add(book2)
+        `when`(bookRepository.findAll()).thenReturn(books)
         assertEquals(2, bookService.findNumberOfBooks())
     }
 }
